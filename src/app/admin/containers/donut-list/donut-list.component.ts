82182db544bbf764df7f50b4e39c49e63e42e051
Pavel <pavlozherebnov@gmail.com>
2023-06-17 19:35:03 +0300

    donut-card component, directives

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -7,12 +7,48 @@ import { Donut } from '../../models';
   styleUrls: ['./donut-list.component.scss']
 })
 export class DonutListComponent {
-  donuts: Array<Donut> = [{
-    id: '1',
-    name: 'Chocolate',
-    icon: 'just-chocolate',
-    price: 119,
-    description: 'Just a choco.'
-  }]
+  donuts: Array<Donut> = [
+    {
+      id: '1',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 119,
+      promo: 'limited',
+      description: 'Just a choco.'
+    },
+    {
+      id: '2',
+      name: 'Caramel Swirl',
+      icon: 'caramel-swirl',
+      price: 129,
+      description: 'Chocolate drizzled with caramel.'
+    },
+    {
+      id: '3',
+      name: 'Sour Supreme',
+      icon: 'sour-supreme',
+      price: 119,
+      promo: 'new',
+      description: 'For the sour advocate.'
+    },
+    {
+      id: '4',
+      name: 'Vanilla Sundae',
+      icon: 'vanilla-sundae',
+      price: 119,
+      description: 'Just a choco.'
+    },
+    {
+      id: '5',
+      name: 'Zesty Lemon',
+      icon: 'zesty-lemon',
+      price: 139,
+      description: 'Delicios lucios lemon.'
+    },
 
+  ]
+
+  trackById(idx: number, value: Donut): string {
+    return value.id
+  }
 }
